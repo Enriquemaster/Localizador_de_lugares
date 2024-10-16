2495f4a0b1b4acd864fcc70cabdbb5a9bd26c016
Enriquemaster <enriquemaster154@gmail.com>
2024-10-16 07:26:52 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Localizador_de_lugares',
+  webDir: 'dist'
+};
+
+export default config;
